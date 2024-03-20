7cc265f4adf4d92dc075ed8cf9fe17eaeb1c454c
rian <rianrgomes@gmail.com>
2024-03-20 14:58:08 -0300

    setup prisma service

--- purchases/src/app.module.ts
+++ purchases/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { HttpModule } from './http/http.module';
-import { PrismaService } from './prisma/prisma.service';
+import { PrismaService } from './database/prisma/prisma.service';
 
 @Module({
   imports: [DatabaseModule, HttpModule],
-  providers: [PrismaService],
 })
 export class AppModule {}
