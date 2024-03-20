730ca3281ccef2f1194ed32a923a414ab1d358ab
rian <rianrgomes@gmail.com>
2024-03-20 11:29:26 -0300

    initialize basic modules on classroom

--- classroom/src/app.module.ts
+++ classroom/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HttpModule } from './http/http.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [HttpModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
