47bac24d48778676b3410031db6dc1aabc47d079
rian <rianrgomes@gmail.com>
2024-03-20 14:50:23 -0300

    first prisma migration

--- purchases/src/app.module.ts
+++ purchases/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { HttpModule } from './http/http.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [DatabaseModule, HttpModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
