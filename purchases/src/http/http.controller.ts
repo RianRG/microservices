6eb10107d97a04c6dcb704e0d485fe6ed0be828e
rian <rianrgomes@gmail.com>
2024-03-20 15:05:16 -0300

    setup graphql

--- purchases/src/http/http.controller.ts
+++ purchases/src/http/http.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, UseGuards } from "@nestjs/common";
 import { AuthorizationGuard } from "./auth/authorization.guard";
+import { PrismaService } from "src/database/prisma/prisma.service";
 
 @Controller('t')
 export class TestController{
+  constructor(private prisma: PrismaService){};
   @Get()
   @UseGuards(AuthorizationGuard)
   hello(){
-    return 'hi'
+    return this.prisma.customer.findMany()
   }
 }
--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config'
 import { TestController } from './http.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import path from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(), 
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql') 
+    })
+  ],
   controllers: [TestController]
 })
 export class HttpModule {}
