a9b9074eb52131a2c772a63c13d64ae42786b9ae
rian <rianrgomes@gmail.com>
2024-03-20 11:27:54 -0300

    create authorization guard with AUTH0

---
+++ purchases/src/http/auth/authorization.guard.ts
@@ -0,0 +1,40 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import jwt from 'express-jwt';
+import { expressJwtSecret } from 'jwks-rsa';
+import { promisify } from 'util';
+import { auth } from 'express-oauth2-jwt-bearer'
+
+@Injectable()
+export class AuthorizationGuard implements CanActivate {
+  private AUTH0_AUDIENCE: string
+  private AUTH0_DOMAIN: string
+  constructor(private configService: ConfigService){
+    this.AUTH0_AUDIENCE = this.configService.get('AUTH0_AUDIENCE') ?? ''
+    this.AUTH0_DOMAIN = this.configService.get('AUTH0_DOMAIN') ?? ''
+  }
+
+  async canActivate(
+    context: ExecutionContext,
+  ): Promise<boolean>{
+    const req = context.switchToHttp().getRequest();
+    const res = context.switchToHttp().getResponse();
+
+    const checkJwt = promisify(
+      auth({
+        audience: 'ignite-login-rian',
+        issuerBaseURL: this.AUTH0_DOMAIN,
+        tokenSigningAlg: 'RS256'
+      })
+    )
+
+    try{
+      await checkJwt(req, res)
+
+      return true
+    } catch(err){
+      throw new UnauthorizedException(err)
+    }
+
+  }
+}
